4c5fc40cf4ea40daf63096b9a22abd52925d4c93
Benjamin <dotsehyde@gmail.com>
2021-05-13 15:01:44 +0000

    Added Contact List

---
+++ lib/widgets/contact_list.dart
@@ -0,0 +1,60 @@
+import 'package:facebook_ui/models/user_model.dart';
+import 'package:facebook_ui/widgets/profile_avatar.dart';
+import 'package:flutter/material.dart';
+
+class ContactList extends StatelessWidget {
+  final List<User> users;
+
+  const ContactList({Key key, this.users}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: const BoxConstraints(maxWidth: 280),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Text(
+                  "Contacts",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: Colors.grey[600],
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              Icon(
+                Icons.search,
+                color: Colors.grey[600],
+              )
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                itemCount: users.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final User user = users[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Row(
+                      children: [
+                        ProfileAvatar(imageUrl: user.imageUrl),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          user.name,
+                          style: TextStyle(fontWeight: FontWeight.w500),
+                        )
+                      ],
+                    ),
+                  );
+                }),
+          )
+        ],
+      ),
+    );
+  }
+}
